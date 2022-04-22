990aded0460154d79b7bab401af9172121cc883e
Katarzyna Kawala <kat.kawala@gmail.com>
2022-04-22 07:55:07 +0200

    adding gitignore and removing target

--- target/classes/db/migration/V4__Create_todo_table_and_data.sql
+++
@@ -1,8 +0,0 @@
-create table todos (
-    id int unsigned primary key auto_increment,
-    text varchar(100) not null,
-    done bit
-);
-
--- insert into todos (text, done) values ('Done todoEntity', 1);
--- insert into todos (text, done) values ('Undone todoEntity', 0);
